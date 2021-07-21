0c4d4558a5f97d9a35772d4c00cf61fa7d3632d5
irfangg <ir786.g@gmail.com>
2021-07-21 18:13:41 +0530

    copy readme to build

--- src/App.js
+++ src/App.js
@@ -18,6 +18,46 @@ function App() {
           Learn React
         </a>
       </header>
+      <div className='outerMost'>
+          <div>Some component</div>
+          <div>One more component</div>
+          <div className='rows'>
+            <div className='row'>
+              <input value='' />
+              <div className='suggestions'>
+                <ul>
+                  <li>option 1</li>
+                  <li>option 1</li>
+                  <li>option 1</li>
+                  <li>option 1</li>
+                </ul>
+              </div>
+            </div>
+            <div className='row'>
+              <input value='' />
+              <div className='suggestions'>
+                <ul>
+                  <li>option 1</li>
+                  <li>option 1</li>
+                  <li>option 1</li>
+                  <li>option 1</li>
+                </ul>
+              </div>
+            </div>
+            <div className='row'>
+              <input value='' />
+              <div className='suggestions'>
+                <ul>
+                  <li>option 1</li>
+                  <li>option 1</li>
+                  <li>option 1</li>
+                  <li>option 1</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div><button>click me</button></div>
+      </div>
     </div>
   );
 }
